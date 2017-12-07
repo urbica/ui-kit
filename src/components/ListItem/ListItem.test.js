ffde5702436a32d5b87b17a1e37105316ad51cf2
Artem Boyur <gboyur@gmail.com>
2017-12-07 19:01:04 +0300

    add test ListItem

--- src/components/ListItem/ListItem.test.js
+++ src/components/ListItem/ListItem.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import ListItem from '../ListItem';
 
 test('ListItem does not crash', () => {
-  const tree = shallow(<ListItem>title</ListItem>);
+  const tree = shallow(<ListItem
+    title="test"
+    description="test"
+    onClick={() => {}}
+  />);
   expect(toJson(tree)).toMatchSnapshot();
+});
+
+test('ListItem test isActive', () => {
+  const wrapper = mount(<ListItem
+    title="test"
+    description="test"
+    onClick={() => {}}
+    isActive={false}
+  />);
+  expect(wrapper.props().isActive).toEqual(false);
+  wrapper.setProps({ isActive: true });
+  expect(wrapper.props().isActive).toEqual(true);
 });
