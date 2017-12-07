7279d1dd686033e02065cef5ca95cb68ab8d5b16
Artem Boyur <gboyur@gmail.com>
2017-12-07 12:09:53 +0300

    new test

--- src/components/Legend/Categorical/Categorical.test.js
+++ src/components/Legend/Categorical/Categorical.test.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import LegendCategorical from './';
 
 const values = [
-  { id: 1, color: '#111111', title: 0 },
-  { id: 2, color: '#333333', title: 100 },
-  { id: 3, color: '#666666', title: 300 },
-  { id: 4, color: '#999999', title: 600 },
-  { id: 5, color: '#AAAAAA', title: 900 }
+  { id: 1, color: '#111111', label: 0 },
+  { id: 2, color: '#333333', label: 100 },
+  { id: 3, color: '#666666', label: 300 },
+  { id: 4, color: '#999999', label: 600 },
+  { id: 5, color: '#AAAAAA', label: 900 }
+];
+
+const valuesNewColors = [
+  { id: 1, color: 'red', label: 0 },
+  { id: 2, color: 'green', label: 100 },
+  { id: 3, color: 'black', label: 300 },
+  { id: 4, color: '#999999', label: 600 },
+  { id: 5, color: '#AAAAAA', label: 900 }
 ];
 
 test('Legend does not crash', () => {
@@ -19,4 +27,11 @@ test('Legend does not crash', () => {
 test('Legend with an empty array', () => {
   expect(() => shallow(<LegendCategorical values={[]} />))
     .toThrowError('values is empty');
+});
+
+test('Legend with an change color', () => {
+  const wrapper = mount(<LegendCategorical values={values} />);
+  expect(wrapper.props().values).toEqual(values);
+  wrapper.setProps({ values: valuesNewColors });
+  expect(wrapper.props().values).toEqual(valuesNewColors);
 });
