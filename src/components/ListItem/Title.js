39134fdd434e064f8d6a8c1460175ba15ee7e1db
Artem Boyur <gboyur@gmail.com>
2017-12-13 13:16:09 +0300

    fix style

--- src/components/ListItem/Title.js
+++ src/components/ListItem/Title.js
@@ -2,7 +2,6 @@ import styled from 'react-emotion';
 
 export default styled.div`
   font-size: 16px;
-  font-weight: ${({ isActive }) => (isActive ? 'bold' : 'inherit')};
   margin: 5px 0;
-  color: ${({ isActive }) => (isActive ? '#303d41' : 'inherit')};
+  color: ${({ isActive }) => (isActive ? '#000000' : 'inherit')};
 `;
