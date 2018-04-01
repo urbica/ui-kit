31d193ac356ea78f829a67e64694c8454bb328e3
boyur <gboyur@gmail.com>
2018-04-01 14:13:44 +0300

    remove scale from RangeSlider

--- src/components/RangeSlider/Scale.js
+++
@@ -1,9 +0,0 @@
-import styled from 'react-emotion';
-
-export default styled.div`
-  display: flex;
-  width: 100%;
-  position: relative;
-  font-size: 14px;
-  justify-content: space-between;
-`;
--- src/components/RangeSlider/Text.js
+++
@@ -1,6 +0,0 @@
-import styled from 'react-emotion';
-
-export default styled.div`
-  display: inline-block;
-  font-weight: ${props => (props.isActive ? 600 : 'normal')};
-`;
