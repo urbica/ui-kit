8a6e60cc3b9a8367b8a64b9a72bb31b1c179aab9
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-05-18 17:11:05 +0300

    feat: export defaul theme

---
+++ src/defaultTheme.js
@@ -0,0 +1,7 @@
+import inputRangeTheme from './components/InputRange/defaultTheme.json';
+import legendRangeTheme from './components/Legend/defaultTheme.json';
+
+export default {
+  ...inputRangeTheme,
+  ...legendRangeTheme
+};
