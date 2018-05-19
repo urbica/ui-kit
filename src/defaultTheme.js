4fd581ac1e6bacb5812bd4f9f17f9cc81f2eaab5
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-05-19 18:02:11 +0300

    feat: add theme support in Button (#53)
    #48 #52

--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -1,7 +1,9 @@
+import buttonTheme from './components/Button/defaultTheme.json';
 import inputRangeTheme from './components/InputRange/defaultTheme.json';
 import legendRangeTheme from './components/Legend/defaultTheme.json';
 
 export default {
+  ...buttonTheme,
   ...inputRangeTheme,
   ...legendRangeTheme
 };
