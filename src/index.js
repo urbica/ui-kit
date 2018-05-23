8389fb1a49990392d5ba503c8bb895638d22ebe5
Anton Ivchenko <a.a.ivchenko@gmail.com>
2018-05-23 13:14:40 +0300

    feat: add ClickOutside to import at index.js

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ export { default as MonthRangePicker } from './components/MonthRangePicker';
 export { default as YearPicker } from './components/YearPicker';
 export { default as Legend } from './components/Legend';
 export { default as RangeSlider } from './components/RangeSlider';
+export { default as ClickOutside } from './components/ClickOutside';
